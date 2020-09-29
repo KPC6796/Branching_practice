ad35fcda10e7530e484385848ffea36105377c92
KPC6796 <54300384+KPC6796@users.noreply.github.com>
2020-09-29 18:01:37 +0530

    Add files via upload

---
+++ branch.py
@@ -0,0 +1,8 @@
+happy = input("Are you happy with your current job?")
+
+if happy == "yes":
+    print("Because of work culture")
+    print("Because of good work-life balance")
+
+else:
+    print("Look for a better opportunity")
