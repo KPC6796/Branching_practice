b5f5178f8b804068e501eccda7cb5444e3c49b25
KPC6796 <kamakshi.dsc@gmail.com>
2020-09-29 18:23:10 +0530

    Added Salary feature

--- branch.py
+++ branch.py
@@ -3,6 +3,7 @@ happy = input("Are you happy with your current job?")
 if happy == "yes":
     print("Because of work culture")
     print("Because of good work-life balance")
+    print("Because of good salary")
 
 else:
     print("Look for a better opportunity")
